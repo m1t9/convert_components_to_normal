e232800e2e0a099ab2a929083ad81d25701d9ece
◉_◉ MITYA ◉_◉ <dpleshanov@gmail.com>
2019-02-28 19:14:00 +0300

    Update velocity.js

--- velocity.js
+++ velocity.js
@@ -9,17 +9,13 @@ module.exports = function (velocity) {
 		getVel: function(ang1, ang2, len) {
 
 			var vel = 0.0;
-
-			// console.log(ang1);
-			// console.log(ang2);
+			
 			ang2 = ang2 - ang1;
 			if (ang2 < 0.0) {
 				ang2 = ang2 + 360.0;
 			}
 			ang1 = 0.0;
 
-			// console.log('ang1 ',ang1);
-			// console.log('ang2', ang2);
 			if (ang2 > 0.0 && ang2 < 90.0) {
 				// part 1
 				vel = Math.sin(ang2 * Math.PI / 180.0) * len;
@@ -32,7 +28,6 @@ module.exports = function (velocity) {
 			} else {
 				vel = -1 * Math.sin((Math.abs(ang2 - 360.0)) * Math.PI / 180.0) * len;
 			}
-			// console.log('vel ', vel);
 
 			return vel;
 
